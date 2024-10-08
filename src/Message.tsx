480559ce93d9fa494ee8838e12186b1cb49cfb5f
Dan Louie <villanuevadan213@gmail.com>
2024-10-08 15:49:16 +0800

    react-app init

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    // Javascript XML
+    const name = 'Dan Louie';
+    if (name)
+    return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
